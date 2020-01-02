3c65bf0806668d85618ad80ef0016ebd0c12f8ea
Sarat Ololade Onanuga <slonanuga@gmail.com>
2020-01-02 17:32:15 +0000

    created script

---
+++ Gemfile
@@ -0,0 +1,14 @@
+
+source 'https://rubygems.org'
+
+
+gem 'nokogiri'   
+gem 'dotenv'    
+gem 'rest-client', '~> 2.1.0'
+gem 'twilio-ruby'
+
+group :development, :test do
+  gem 'rspec' 
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'webmock'
+end
---
+++ lib/ra_resale_checker.rb
@@ -0,0 +1,25 @@
+require 'twilio-ruby'
+require 'nokogiri'    
+require 'rest-client'
+
+class RaResaleChecker
+
+  def run(url)
+    send_text(@event_name) if resale_ticket_available?
+  end
+
+  def resale_ticket_available?
+    parsed_doc = Nokogiri::HTML(RestClient.get(url))
+    RestClient.get(url)
+    @event_name = parsed_doc.search('#sectionHead h1').text
+    parsed_doc.search('#buynow').present?
+  end
+
+  def send_text(event_name)
+    client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
+    client.messages.create(
+        body: "A resale ticket is now available for #{event_name}"
+        to: ENV['MOBILE_NUM'],
+        from: ENV['TWILIO_NUM'])
+  end
+end
