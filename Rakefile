ecfc2b59c8169a7890899294cc4554569236e4aa
Sarat Ololade Onanuga <slonanuga@gmail.com>
2020-01-03 10:01:48 +0000

    rake task now runs

--- Rakefile
+++ Rakefile
@@ -1,5 +1,9 @@
+
+require_relative 'lib/ra_resale_checker'
+
 desc "Task to run the Resale checker at 30 minute intervals. Enter desired ticket link"
-task :check_for_tickets => :environment do
+task :check_for_tickets do
 RaResaleChecker.new("https://www.residentadvisor.net/events/1352302").run
 end
+
 
--- lib/ra_resale_checker.rb
+++ lib/ra_resale_checker.rb
@@ -1,6 +1,7 @@
 require 'twilio-ruby'
 require 'nokogiri'    
 require 'rest-client'
+require 'byebug'
 
 class RaResaleChecker
 
@@ -13,18 +14,19 @@ class RaResaleChecker
       send_text(@event_name)
     else
       puts 'no tickets available'
+    end
   end
 
   def resale_ticket_available?
     parsed_doc = Nokogiri::HTML(RestClient.get(@url))
     @event_name = parsed_doc.search('#sectionHead h1').text
-    parsed_doc.search('#buynow').present?
+    parsed_doc.search('#buynow').any?
   end
 
   def send_text(event_name)
     client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
     client.messages.create(
-        body: "A resale ticket is now available for #{event_name}"
+        body: "A resale ticket is now available for #{event_name}",
         to: ENV['MOBILE_NUM'],
         from: ENV['TWILIO_NUM'])
   end
